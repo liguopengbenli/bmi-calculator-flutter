290767376bb80f1e05f5752c47e03b7a357d2808
guopeng li <guopeng.li@technicolor.com>
2021-01-28 11:45:55 +0100

    put all constant in a separated class named constant and update UI

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kbottomContainerHeight = 80.0;
+const kactiveCardColor = Color(0XFF1D1E33);
+const kinactiveCardColor = Color(0XFF111328);
+const kbottomColor = Color(0XFFEB1555);
+
+const klabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,11 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'Icon_content.dart';
-
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0XFF1D1E33);
-const inactiveCardColor = Color(0XFF111328);
-const bottomColor = Color(0XFFEB1555);
+import 'constants.dart';
 
 enum Gender { male, female }
 
@@ -26,6 +22,7 @@ class _InputPageState extends State<InputPage> {
           title: Text('BMI CALCULATOR'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
                 child: Row(
@@ -33,8 +30,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                       selectedGender == Gender.male
-                          ? activeCardColor
-                          : inactiveCardColor,
+                          ? kactiveCardColor
+                          : kinactiveCardColor,
                       ContentWidget(FontAwesomeIcons.mars, "'MALE'"), () {
                     setState(() {
                       selectedGender = Gender.male;
@@ -44,8 +41,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                       selectedGender == Gender.female
-                          ? activeCardColor
-                          : inactiveCardColor,
+                          ? kactiveCardColor
+                          : kinactiveCardColor,
                       ContentWidget(FontAwesomeIcons.venus, "'FEMALE'"), () {
                     setState(() {
                       selectedGender = Gender.female;
@@ -54,19 +51,42 @@ class _InputPageState extends State<InputPage> {
                 ),
               ],
             )),
-            Expanded(child: ReusableCard(activeCardColor)),
+            Expanded(
+                child: ReusableCard(
+              kactiveCardColor,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'HEIGHT',
+                    style: klabelTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    children: <Widget>[
+                      Text(
+                        '180',
+                        style: kNumberTextStyle,
+                      ),
+                      Text('cm', style: klabelTextStyle)
+                    ],
+                  )
+                ],
+              ),
+            )),
             Expanded(
                 child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(activeCardColor)),
-                Expanded(child: ReusableCard(activeCardColor))
+                Expanded(child: ReusableCard(kactiveCardColor)),
+                Expanded(child: ReusableCard(kactiveCardColor))
               ],
             )),
             Container(
               margin: EdgeInsets.only(top: 10.0),
-              color: bottomColor,
+              color: kbottomColor,
               width: double.infinity,
-              height: bottomContainerHeight,
+              height: kbottomContainerHeight,
             )
           ],
         ));
